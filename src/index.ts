4722e2cab3c3556051e9b718ecb163eebe441b53
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2024-04-27 13:11:57 -0400

    Redis and BullMQ infra is up and running

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import express, {Express} from 'express';
+import express, { Express } from "express";
 
-import serverConfig from './config/server.config';
-import apiRouter from './routes';
+import serverConfig from "./config/server.config";
+import sampleQueueProducer from "./producers/samplequeue.producer";
+import apiRouter from "./routes";
+import SampleWorker from "./workers/sample.workers";
+
 
 const app: Express = express();
 
 app.use('/api', apiRouter);
 
-app.listen(serverConfig.PORT, () =>  console.log(`Server started at *:${serverConfig.PORT}`));
+app.listen(serverConfig.PORT, () => {
+  console.log(`Server started at *:${serverConfig.PORT}`);
+
+  SampleWorker('SampleQueue');
+
+  sampleQueueProducer('SampleJob', {
+    name: "Goldy",
+    company: "40Bears",
+    position: "SDE",
+    locatiion: "Remote | Pune | Toronto"
+  });
+});
